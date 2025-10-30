99fb767dcdc98d6d430f44315f377413e69c0ed7
NachitaAB <ignaciaabarcabravo@gmail.com>
2025-10-29 22:12:51 -0300

    Add header file for validation functions

---
+++ validaciones.h
@@ -0,0 +1,9 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+int validarRut(const char *rut);
+int validarFecha(const char *fecha);
+int validarEmail(const char *email);
+void limpiar_buffer(void);
+
+#endif
