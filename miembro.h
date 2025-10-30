3394f5fc8723a52a5c8845efb8ad0c406ba82cb8
NachitaAB <ignaciaabarcabravo@gmail.com>
2025-10-29 21:46:20 -0300

    Add miembro.h header file with Miembro struct

---
+++ miembro.h
@@ -0,0 +1,21 @@
+#ifndef MIEMBRO_H
+#define MIEMBRO_H
+#include "constante.h"
+
+
+typedef struct {
+    char nombre[20];
+    char apellido[20];
+    char rut[13];               // "12345678-9"
+    char direccion[60];         
+    int edad;
+    char fechaNacimiento[11];   // "dd/mm/aaaa"
+    char sexo;                  
+    int casado;                 
+    char email[40];
+} Miembro;
+
+int crearMiembro(Miembro miembro[], int *total);
+int menuMiembro(Miembro miembro[], int *total);
+
+#endif
