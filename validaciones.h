81085039e0d034caae2106ca0df70f3a70202e0e
NachitaAB <ignaciaabarcabravo@gmail.com>
2025-10-30 00:06:10 -0300

    Add Fecha struct to validaciones.h
    Added a struct for representing dates in the validaciones.h file.

--- validaciones.h
+++ validaciones.h
@@ -1,6 +1,12 @@
 #ifndef VALIDACIONES_H
 #define VALIDACIONES_H
 
+typedef struct {
+    int dia;
+    int mes;
+    int anio;
+} Fecha;
+
 int validarRut(const char *rut);
 int validarFecha(const char *fecha);
 int validarEmail(const char *email);
