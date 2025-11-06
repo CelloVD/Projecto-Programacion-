51864d816f5f5611ba4fc82563e7d9e6451e458e
NachitaAB <ignaciaabarcabravo@gmail.com>
2025-11-06 00:17:00 -0300

    Create caja.h

---
+++ caja.h
@@ -0,0 +1,11 @@
+#ifndef CAJA_H
+#define CAJA_H
+
+#define NUM_CAJA 11
+
+extern const char* CAJA_VALIDOS[NUM_CAJA];
+
+int esCajaValida(const char* caja);
+void seleccionarCaja(char destino[], int tam);
+
+#endif
