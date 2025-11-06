4e394736b8edb830bc420bf94ce24e84f03a1798
marti_alcaino <63439337+inexistencia1994@users.noreply.github.com>
2025-11-06 05:04:43 -0300

    Create bloomer.h
    Se crea archivo bloomer para un solo include en el main.

---
+++ bloomer.h
@@ -0,0 +1,43 @@
+#ifndef BLOOMER_H
+#define BLOOMER_H
+
+//LIBRERÍAS ESTÁNDAR
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+
+//CONSTANTES Y VALIDACIONES GLOBALES
+#include "constante.h"
+#include "validaciones.h"
+
+//MÓDULO 1: MIEMBROS Y MENÚ PRINCIPAL
+#include "miembro.h"
+
+//MÓDULO 2: FINANZAS
+#include "finanzas.h"
+#include "banco.h"
+#include "caja.h"
+
+//MÓDULO 3: PROPIEDADES
+#include "propiedades.h"
+
+//MÓDULO 4: VEHÍCULOS
+#include "vehiculo.h"
+
+//MÓDULO 5: FONDOS MUTUOS
+#include "fondosmutuos.h"
+
+//MÓDULO 6: TERRENOS
+#include "terrenos.h"
+
+//MÓDULO 7: AGENDA
+#include "agenda.h"
+
+//MÓDULO 8: BENEFICIOS
+#include "beneficios.h"
+#include "beneficio.h"
+
+#endif // BLOOMER_H
